84dbb775cf3dd01a39d753edcb159020c2830f79
Roman Nikitchenko <roman.nikitchenko@philips.com>
2020-04-09 23:36:25 +0200

    Finally got everything working together with smooth enough process.

--- swift/bin/launch.sh
+++ swift/bin/launch.sh
@@ -8,36 +8,26 @@ service swift-container start
 service swift-account start
 service swift-object start
 
-if [ -z "$KS_SWIFT_PUBLIC_URL" ]; then
-    echo "KS_SWIFT_PUBLIC_URL is required"
-    exit 1
+apachectl -DFOREGROUND &
+
+if [ -z "$KS_SWIFT_PUBLIC_URL" ]
+then
+    ecport KS_SWIFT_PUBLIC_URL="http://127.0.0.1:8086"
 fi
 
 CONFIG_PUB_URL=$(cat /opt/SWIFT_INIT)
-
-if [ "$KS_SWIFT_PUBLIC_URL" = "$CONFIG_PUB_URL" ]; then
+if [ "$KS_SWIFT_PUBLIC_URL" = "$CONFIG_PUB_URL" ]
+then
     echo "$KS_SWIFT_PUBLIC_URL endpoint already configured."
+    sleep 2
 else
-    echo "Configuring $KS_SWIFT_PUBLIC_URL endpoint."
-
-    echo "Starting apache."
-    apachectl start
-    echo "Apache started."
-
-    openstack endpoint create --region RegionOne object-store internal http://127.0.0.1:8080/v1/KEY_%\(tenant_id\)s
-    openstack endpoint create --region RegionOne object-store admin http://127.0.0.1:8080/v1
+    wait_seconds=5
+    until test $((wait_seconds--)) -eq 0 -o -f "$pid_file" ; do echo -n "$wait_seconds " ; sleep 1; done
+    echo
+    openstack endpoint create --region RegionOne object-store internal http://127.0.0.1:8086/v1/KEY_%\(tenant_id\)s
+    openstack endpoint create --region RegionOne object-store admin http://127.0.0.1:8086/v1
     openstack endpoint create --region RegionOne object-store public ${KS_SWIFT_PUBLIC_URL}/v1/KEY_%\(tenant_id\)s
-
-    echo "Stopping apache..."
-    apachectl stop
-    wait_seconds=10
-    pid_file="/var/run/apache2/apache2.pid"
-    until test $((wait_seconds--)) -eq 0 -o ! -f "$pid_file" ; do sleep 1; done
-    echo "Apache is stopped."
 
     echo "$KS_SWIFT_PUBLIC_URL" > /opt/SWIFT_INIT
 fi
-
-echo "Starting apache in foreground."
-
-apachectl -DFOREGROUND
+wait
